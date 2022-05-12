ae7a554f86a7efcac6c568f99207d6c5f830be80
aiden_lu <aiden_lu@wochacha.com>
2022-05-12 20:37:00 +0800

    Bump version to 1.3.12

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_long_desc():
 
 setuptools.setup(
     name="drf_api_logger",
-    version="1.3.11",
+    version="1.3.12",
     author="Vishal Anand",
     author_email="vishalanandl177@gmail.com",
     description="An API Logger for your Django Rest Framework project.",
@@ -29,14 +29,20 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/vishalanandl177/DRF-API-Logger",
     packages=setuptools.find_packages(),
-    install_requires=["djangorestframework>=3.7.4", "bleach>=3.1.5", "user-agents==2.2.0"],
-    license='MIT',
-    python_requires='>=3.7',
+    install_requires=[
+        "djangorestframework>=3.7.4",
+        "bleach>=3.1.5",
+        "user-agents~=2.2.0",
+        "pygments~=2.8.1",
+        "requests~=2.25.1",
+    ],
+    license="MIT",
+    python_requires=">=3.7",
     include_package_data=True,
     classifiers=[
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
     ],
 )
