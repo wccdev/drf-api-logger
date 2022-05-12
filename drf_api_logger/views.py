7afdea5656a2e7e29b12be2f4d6f652c0c34f72c
aiden_lu <aiden_lu@wochacha.com>
2022-05-12 20:08:15 +0800

    Bump version to 1.3.10

--- drf_api_logger/views.py
+++ drf_api_logger/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAdminUser
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from drf_api_logger.models import APILogsModel
@@ -9,6 +10,7 @@ class APILoggerViewSet(ReadOnlyModelViewSet):
     A simple ViewSet for viewing api logs
     """
     queryset = APILogsModel.objects.select_related('request_user')
+    permission_classes = (IsAdminUser,)
 
     def get_serializer_class(self):
         if self.action == "list":
